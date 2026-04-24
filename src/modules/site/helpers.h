0638838d1e68707bcfe6e1cfb4cdea6ad474a2b9
Peter Kuehne <peter.kuehne@gmail.com>
2026-04-24 11:25:36 +0100

    Fix formatting

--- src/modules/site/helpers.h
+++ src/modules/site/helpers.h
@@ -5,10 +5,10 @@
 #include <string>
 
 flecs::entity instantiateBuilding(flecs::world &world, const std::string &name,
-                                  const std::string &prefab, uint32_t x, uint32_t y,
-                                  flecs::entity site);
-flecs::entity instantiateConstructionSite(flecs::world &world, uint32_t x, uint32_t y,
-                                          flecs::entity site);
+                                  const std::string &prefab, uint32_t x,
+                                  uint32_t y, flecs::entity site);
+flecs::entity instantiateConstructionSite(flecs::world &world, uint32_t x,
+                                          uint32_t y, flecs::entity site);
 flecs::entity instantiateBuildingNotification(flecs::world &world,
                                               flecs::entity building,
                                               const std::string &text);
