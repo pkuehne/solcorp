14b1211a314deaf4af0a2a51037fc313f91de5ef
Peter Kuehne <peter.kuehne@gmail.com>
2026-05-14 12:21:46 +0100

    Add cheats menu to Developer Window

--- src/modules/simulation/developer_window.cpp
+++ src/modules/simulation/developer_window.cpp
@@ -1,8 +1,11 @@
 #include "developer_window.h"
 #include "imgui.h"
 #include "modules/engine/gui.h"
+#include "modules/simulation/simulation.h"
 #include <flecs.h>
 #include <spdlog/spdlog.h>
+
+void drawCheatsTab(flecs::world &);
 
 void showDeveloperWindow(flecs::world &world) {
   showWindow(world, "Developer Window");
@@ -23,12 +26,24 @@ void drawDeveloperWindow(flecs::entity winE) {
   ImGui::Checkbox("Show Metrics Window", &state.show_metrics_window);
 
   if (ImGui::BeginTabBar("Tools")) {
-    if (ImGui::BeginTabItem("Entity Tree")) {
+    if (ImGui::BeginTabItem("Cheats")) {
+      drawCheatsTab(world);
+      ImGui::EndTabItem();
     }
-    world.children([](flecs::entity e) { child_tree(e); });
+    if (ImGui::BeginTabItem("Entity Tree")) {
+      world.children([](flecs::entity e) { child_tree(e); });
+      ImGui::EndTabItem();
+    }
+    ImGui::EndTabBar();
   }
 
   if (state.show_metrics_window) {
     ImGui::ShowMetricsWindow();
+  }
+}
+
+void drawCheatsTab(flecs::world &world) {
+  if (ImGui::Button("Add $10M to balance")) {
+    world.get_mut<Company>().balance += 10'000'000;
   }
 }
