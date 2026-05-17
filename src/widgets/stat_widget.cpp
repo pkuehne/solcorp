bc48caabb093473a9cddeba8bd140d6c512baa20
Peter Kuehne <peter.kuehne@gmail.com>
2026-05-17 20:23:23 +0100

    feat(widget): Allow passing custom formatter to StatTooltip

--- src/widgets/stat_widget.cpp
+++ src/widgets/stat_widget.cpp
@@ -1,26 +1,28 @@
 #include "stat_widget.h"
 #include "imgui.h"
 #include <format>
+#include <functional>
 
 namespace Widgets {
 
-void StatTooltip(const Stat *stat) {
-  ImGui::Text("%s: %.0f", stat->display().c_str(), stat->value());
+void StatTooltip(const Stat *stat,
+                 const std::function<std::string(double)> &fmt) {
+  ImGui::Text("%s: %s", stat->display().c_str(), fmt(stat->value()).c_str());
 
   if (ImGui::BeginItemTooltip()) {
     ImGui::Text("%s", stat->description().c_str());
     ImGui::Separator();
-    ImGui::Text("Base Value: %.0f", stat->base());
+    ImGui::Text("Base Value: %s", fmt(stat->base()).c_str());
     constexpr ImVec4 red = ImVec4(1.0, 0.0, 0.0, 1.0);
     constexpr ImVec4 green = ImVec4(0.0, 0.5, 0.0, 1.0);
     for (const auto &item : stat->modifiers()) {
       std::string modValue = "";
       ImVec4 colour;
       if (item.mod.additive > 0) {
-        modValue = std::format("+{:.0f}", item.mod.additive);
+        modValue = "+" + fmt(item.mod.additive);
         colour = stat->isHigherBetter() ? green : red;
       } else if (item.mod.additive < 0) {
-        modValue = std::format("{:.0f}", item.mod.additive);
+        modValue = fmt(item.mod.additive);
         colour = stat->isHigherBetter() ? red : green;
       }
       if (item.mod.multiplicative > 1) {
@@ -35,7 +37,7 @@ void StatTooltip(const Stat *stat) {
       ImGui::TextColored(colour, "%s", modValue.c_str());
     }
     ImGui::Separator();
-    ImGui::Text("Final Value: %.0f", stat->value());
+    ImGui::Text("Final Value: %s", fmt(stat->value()).c_str());
     ImGui::EndTooltip();
   }
 }
