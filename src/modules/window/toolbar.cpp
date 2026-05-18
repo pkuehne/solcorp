22d6ddf6713d753e8fd01e52c2152ffaa4a92245
Peter Kuehne <peter.kuehne@gmail.com>
2026-05-18 21:07:48 +0100

    Make toolbar use FontAwesome

--- src/modules/window/toolbar.cpp
+++ src/modules/window/toolbar.cpp
@@ -3,6 +3,7 @@
 #include "imgui_internal.h"
 #include <modules/rocket/active_launches_window.h>
 #include <modules/rocket/contracts_window.h>
+#include <modules/window/notification_window.h>
 
 void systemDrawToolbar(flecs::entity winE, Toolbar) {
   auto world = winE.world();
@@ -16,13 +17,24 @@ void systemDrawToolbar(flecs::entity winE, Toolbar) {
   if (ImGui::BeginViewportSideBar("##Toolbar", viewport, ImGuiDir_Up, height,
                                   toolbar_flags)) {
     if (ImGui::BeginMenuBar()) {
-      if (ImGui::SmallButton("Launches")) {
+      if (ImGui::Button("\xef\x84\xb5")) { // fa-rocket f135
         showActiveLaunchesWindow(world);
       }
+      ImGui::SetItemTooltip("Launches");
       ImGui::SameLine();
-      if (ImGui::SmallButton("Contracts")) {
+      if (ImGui::Button("\xef\x95\xac")) { // fa-file-contract f56c
         showContractsWindow(world);
       }
+      ImGui::SetItemTooltip("Contracts");
+
+      static const char *envelope = "\xef\x83\xa0"; // fa-envelope f0e0
+      float envelope_width = ImGui::CalcTextSize(envelope).x +
+                             ImGui::GetStyle().FramePadding.x * 2;
+      ImGui::SetCursorPosX(ImGui::GetContentRegionMax().x - envelope_width);
+      if (ImGui::Button(envelope)) {
+        showNotificationWindow(world);
+      }
+      ImGui::SetItemTooltip("Notifications");
       ImGui::EndMenuBar();
     }
   }
