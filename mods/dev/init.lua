69bf962c49a3674a60b312cd0fff566a5e3ff296
Peter Kuehne <peter.kuehne@gmail.com>
2026-06-19 18:03:34 +0100

    split dev content into dev mod

--- mods/dev/init.lua
+++ mods/dev/init.lua
@@ -1,6 +1,72 @@
 local function on_start()
 	local info = solcorp.logging.info
 	info("on_start called!")
+	local site = solcorp.entities.get("Sun::Earth::Cape Canaveral")
+	if site == nil then
+		info("Cape Canaveral not found!")
+	else
+		info("Cape Canaveral: " .. tostring(site:is_alive()))
+	end
+
+	-- Add a road connector
+	local ConnectorVariant = solcorp.constants.ConnectorVariant
+	for i = 0, 9 do
+		solcorp.helpers.create_connector(
+			"Road 3," .. i,
+			ConnectorVariant.Straight,
+			0,
+			3,
+			i,
+			site,
+			"roads_asphalt",
+			"roads_markings"
+		)
+	end
+
+	-- Add some buildings to the site
+	solcorp.helpers.create_building("Manufacturing A", "Factory", 1, 1, site)
+	local storage = solcorp.helpers.create_building("Storage Hall 1", "Storage Hall", 1, 0, site)
+	local main_launchpad = solcorp.helpers.create_building("Main Launchpad", "Launch Complex", 8, 5, site)
+	solcorp.helpers.create_building("North Building", "Office Building", 0, 6, site)
+
+	-- Add some modifiers to the launchpad
+	local mod = solcorp.components.Modifier:new()
+	mod.target_stat = "max-weight"
+
+	local concrete = solcorp.helpers.create_effect("Better Concrete", site)
+	mod.multiplicative = 1.2
+	solcorp.helpers.add_modifier(concrete, mod)
+
+	local cracks = solcorp.helpers.create_effect("Cracks detected", main_launchpad)
+	mod.multiplicative = 0.4
+	solcorp.helpers.add_modifier(cracks, mod)
+
+	local struts = solcorp.helpers.create_effect("Reinforcing Struts", site)
+	mod.multiplicative = 1.0
+	mod.additive = 500
+	solcorp.helpers.add_modifier(struts, mod)
+
+	-- Create a rocket prefab
+	local rocket = solcorp.helpers.create_rocket_prefab("Falcon 1")
+	rocket:getRocket().cost.base = 5 * 1000 * 1000
+	solcorp.helpers.add_target_orbit_to_rocket(rocket, "Sun::Earth::Low Orbit", 6300)
+	solcorp.helpers.add_target_orbit_to_rocket(rocket, "Sun::Earth::Polar Orbit", 5600)
+	solcorp.helpers.add_target_orbit_to_rocket(rocket, "Sun::Earth::Transfer Orbit", 3300)
+	solcorp.helpers.add_target_orbit_to_rocket(rocket, "Sun::Earth::Synchronous Orbit", 1300)
+
+	-- Create a rocket
+	local hall = storage:lookup("Hall 1")
+	solcorp.helpers.create_rocket("Falcon 1 - Unit 001", "Falcon 1", hall)
+
+	-- Create a contract
+	local contract = solcorp.helpers.create_contract(
+		"Your first contract",
+		"Angel Investors Inc.",
+		"Launch your first satellite into low Earth orbit.",
+		2 * 1000 * 1000,
+		2 * 1000 * 1000
+	)
+	solcorp.helpers.create_contract_payload(contract, "Inaugural 1", 800, "Sun::Earth::Low Orbit")
 end
 
 solcorp.script.handlers.on_init = nil
